f9795b3abb3b9747ef11a404e2859d8a6f925b7c
Abilash Ananthula <consult.abilash@gmail.com>
2022-11-28 07:56:35 +0530

    for render deploy

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 // the __dirname is the current directory from where the script is // running
 var __dirname = "";
 
-app.use(express.static("./"));
+app.use(express.static("./build"));
 // send the user to index html page inspite of the url
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve("./", "index.html"));
+  res.sendFile(path.resolve("./build/", "index.html"));
 });
 app.listen(port);
